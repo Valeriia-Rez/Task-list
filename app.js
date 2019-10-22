32a7fe1fdb3874c0a5985a7c8687eb253776ea1a
Valeriia-Rez <Valeria09122016@gmail.com>
2019-10-22 23:34:52 +0300

    change app.js,create delete and filter tasks

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ const taskInput = document.querySelector("#task");
 loadEventListeners();
 
 //Load all event listeners
-function loadEventListeners(){
-    //Add task event
+function loadEventListeners() {
+//Add task event
     form.addEventListener("submit", addTask);
+//Remove task event
+taskList.addEventListener("click", removeTask);
+//Clear task event
+clearBtn.addEventListener("click", clearTasks);
+//Filter tasks event
+filter.addEventListener("keyup", filterTasks);
 }
 
-    //Add task
+//Add task
 function addTask(e){
     if(taskInput.value === ""){
         alert("Add a task");
@@ -37,9 +43,42 @@ li.appendChild(link);
 taskList.appendChild(li);
 
 //Clear input
-taskInput.value = "";
+taskInput.value = " ";
 
 console.log(li);
 
     e.preventDefault();
+}
+
+//Remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains("delete-item")) {
+        if(confirm("Are you sure?")) {
+        e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+function clearTasks() {
+    //taskList.innerHTML = " ";
+    //Faster
+  while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+        //http://jsperf.com/innerHTML-vs-removeChild
+    }
+}
+
+//Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll(".collection-item").forEach
+    (function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = "block";
+        } else{
+            task.style.display = "none";
+        }
+     });
 }
